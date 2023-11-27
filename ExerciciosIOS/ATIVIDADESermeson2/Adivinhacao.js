7cff11738a4aa4cd0cdbb7434b5bcb1c14c471ad
Kaique de Lima Romão <143458223+KaiqueLimaRomao@users.noreply.github.com>
2023-11-27 01:15:08 -0300

    Add files via upload

---
+++ ExerciciosIOS/ATIVIDADESermeson2/Adivinhacao.js
@@ -0,0 +1,9 @@
+const numerosorteado = 9;
+
+let numerousuario = prompt("Digite um número de 0 a 10");
+
+if (numerousuario== numerosorteado) {
+    alert ("Parábens!!!, você acertou o número sorteado")
+} else {
+    alert ("Tente novamnte")
+}
---
+++ ExerciciosIOS/JS/DOM2.js
@@ -0,0 +1,11 @@
+const numeroSorteado = 9;
+
+function Sorteio() {
+    let NumeroUsuario = prompt("Digite um número de 0 a 10");
+
+if (NumeroUsuario == 9) {
+    prompt("Parábens!!, você acertou o numero sorteado")    
+} else {
+    prompt ("Você não acertou, tente novamente")
+}
+}
